ac720f3647d879949ab5344124886814040d238d
bbll <768963019@QQ.com>
2023-10-29 21:13:36 +0800

    Update mpconfigboard.h

--- ports/espressif/boards/ai_thinker_esp_12h_nodemcu/mpconfigboard.h
+++ ports/espressif/boards/ai_thinker_esp_12h_nodemcu/mpconfigboard.h
@@ -32,3 +32,15 @@
 #define MICROPY_HW_MCU_NAME         "ESP32S2"
 
 // #define MICROPY_HW_NEOPIXEL (&pin_GPIO18)
+// Status LED
+#define CIRCUITPY_RGB_STATUS_R      (&pin_GPIO9)
+#define CIRCUITPY_RGB_STATUS_G      (&pin_GPIO11)
+#define CIRCUITPY_RGB_STATUS_B      (&pin_GPIO12)
+
+// Default bus pins
+#define DEFAULT_UART_BUS_RX         (&pin_GPIO20)
+#define DEFAULT_UART_BUS_TX         (&pin_GPIO21)
+
+// Serial over UART
+#define CIRCUITPY_CONSOLE_UART_RX               DEFAULT_UART_BUS_RX
+#define CIRCUITPY_CONSOLE_UART_TX               DEFAULT_UART_BUS_TX
