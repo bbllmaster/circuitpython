3e2f2ab5c2d48494c6ee01b4107d50c0097b96ed
bbll <768963019@QQ.com>
2023-10-29 19:17:03 +0800

    Add files via upload

--- ports/espressif/boards/ai_thinker_esp_12k_nodemcu/mpconfigboard.h
+++ ports/espressif/boards/ai_thinker_esp_12k_nodemcu/mpconfigboard.h
@@ -28,7 +28,7 @@
 
 // Same setup as the Saola board but with no Neopixel on board
 
-#define MICROPY_HW_BOARD_NAME       "ESP 12k NodeMCU"
+#define MICROPY_HW_BOARD_NAME       "ESP 12h NodeMCU"
 #define MICROPY_HW_MCU_NAME         "ESP32S2"
 
 // #define MICROPY_HW_NEOPIXEL (&pin_GPIO18)
