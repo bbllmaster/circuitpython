a513342ffbb0486c9da727fd875b5923b6ca0246
bbll <768963019@QQ.com>
2023-10-29 19:12:37 +0800

    Add files via upload

--- ports/espressif/boards/adafruit_feather_esp32s3_nopsram/mpconfigboard.h
+++ ports/espressif/boards/adafruit_feather_esp32s3_nopsram/mpconfigboard.h
@@ -26,22 +26,17 @@
 
 // Micropython setup
 
-#define MICROPY_HW_BOARD_NAME       "Adafruit Feather ESP32S3 No PSRAM"
+#define MICROPY_HW_BOARD_NAME       "Adafruit Feather ESP32-S3 TFT"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
-#define MICROPY_HW_NEOPIXEL (&pin_GPIO33)
-#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO21)
-
-#define MICROPY_HW_LED_STATUS (&pin_GPIO13)
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO48)
 
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO4)
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO3)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO5)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_GPIO36)
-#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO35)
-#define DEFAULT_SPI_BUS_MISO (&pin_GPIO37)
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO12)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO11)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO45)
 
-#define DEFAULT_UART_BUS_RX (&pin_GPIO38)
-#define DEFAULT_UART_BUS_TX (&pin_GPIO39)
-
-#define DOUBLE_TAP_PIN (&pin_GPIO34)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
