3b6743aa137d109019d155cbdbf76334e8d9b190
bbll <768963019@QQ.com>
2023-11-09 08:40:40 +0800

    Update board.c

--- ports/espressif/boards/adafruit_feather_esp32s3_tft_169/board.c
+++ ports/espressif/boards/adafruit_feather_esp32s3_tft_169/board.c
@@ -94,7 +94,7 @@ void board_init(void) {
         280,            // width (after rotation)
         240,            // height (after rotation)
         20,             // column start
-        20,             // row start
+        0,             // row start
         0,              // rotation
         16,             // color depth
         false,          // grayscale
