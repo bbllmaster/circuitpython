2761ca2a0375d7b87ffa328203960f497575436e
bbll <768963019@QQ.com>
2025-04-13 16:51:58 +0800

    Add files via upload

--- ports/raspberrypi/boards/adafruit_feather_rp2040_dvi/board.c
+++ ports/raspberrypi/boards/adafruit_feather_rp2040_dvi/board.c
@@ -1,8 +1,28 @@
-// This file is part of the CircuitPython project: https://circuitpython.org
-//
-// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
-//
-// SPDX-License-Identifier: MIT
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "supervisor/board.h"
 
@@ -14,10 +34,10 @@ void board_init(void) {
     picodvi_framebuffer_obj_t *fb = &allocate_display_bus()->picodvi;
     fb->base.type = &picodvi_framebuffer_type;
     common_hal_picodvi_framebuffer_construct(fb, 320, 240,
-        &pin_GPIO17, &pin_GPIO16,
-        &pin_GPIO19, &pin_GPIO18,
-        &pin_GPIO21, &pin_GPIO20,
-        &pin_GPIO23, &pin_GPIO22,
+        &pin_GPIO1, &pin_GPIO0,
+        &pin_GPIO3, &pin_GPIO2,
+        &pin_GPIO5, &pin_GPIO4,
+        &pin_GPIO7, &pin_GPIO6,
         8);
 
     framebufferio_framebufferdisplay_obj_t *display = &allocate_display()->framebuffer_display;
